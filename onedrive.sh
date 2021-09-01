c16baa5f8f7e57f91d6fa80214885efb492d47c4
Yaya.Cout <yaya.cout@free.fr>
2021-09-01 18:50:01 +0200

    Update onedrive.sh

--- onedrive.sh
+++ onedrive.sh
@@ -2,9 +2,10 @@ bashincat=$(cat ~/OneDrive/bash.sh)
 while true
 do
 #	onedrive --check-for-nosync --synchronize --disable-notifications # > /dev/null
-	if [ "$bashincat" -ne $(cat ~/OneDrive/bash.sh) ]
+	if [ "$bashincat" != "$(cat ~/OneDrive/bash.sh)" ]
 	then
 		bash -c "~/OneDrive/bash.sh < ~/OneDrive/stdin.sh 1> tee ~/OneDrive/bashstdout.sh 2> bashstderr"
+		bashincat=$(cat ~/OneDrive/bash.sh)
 	fi
 	sleep 1
 done
